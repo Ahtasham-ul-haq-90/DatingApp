8d9f638af57722c3e7788713104e9f3cb613099d
Ahtasham-ul-haq-90 <Ahtashamulhaq198@gmail.com>
2025-07-30 18:17:35 +0500

    Initial migration

---
+++ API/DatingAPP_API/Entities/AppUser.cs
@@ -0,0 +1,6 @@
+public class AppUser
+{
+    public Guid Id { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+}
--- API/DatingAPP_API/Program.cs
+++ API/DatingAPP_API/Program.cs
@@ -1,7 +1,15 @@
+using DatingAPP_API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+
 var builder = WebApplication.CreateBuilder(args);
-
+var connectionString = builder.Configuration.GetConnectionString("ConnectionString");
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
