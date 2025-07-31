82f224932d669c8a5997e8847f94d12c341d444f
Ahtasham-ul-haq-90 <Ahtashamulhaq198@gmail.com>
2025-07-31 14:26:39 +0500

    Removed angular project from this repo

---
+++ API/DatingAPP_API/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using DatingAPP_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAPP_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> Get() { 
+        return await _context.Users.ToListAsync();
+        }
+    }
+}
